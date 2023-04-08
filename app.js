9d55b04037af380603ed8d00dabbc013fd9242da
ray199229 <ray199229@gmail.com>
2023-04-08 17:30:46 +0800

    feat: add signin page

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 
 const app = express()
 const PORT = 3000
 
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.render('signin')
 })
 
 app.listen(PORT, () => {
