d78aab31971d745ee55ee15e20bd642c64b04a50
ray199229 <ray199229@gmail.com>
2023-04-08 17:58:11 +0800

    feat: add routes and sequelize

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
 
+const { apis, pages } = require('./routes')
+
 const app = express()
 const PORT = 3000
 
@@ -8,9 +10,8 @@ app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
-app.get('/', (req, res) => {
-  res.render('signin')
-})
+app.use('/apis', apis)
+app.use(pages)
 
 app.listen(PORT, () => {
   console.log(`Express app is running on http://localhost:${PORT}`)
