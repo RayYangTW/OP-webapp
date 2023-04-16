6cc0128530d5631e839fa99c6509e1c908717fff
ray199229 <ray199229@gmail.com>
2023-04-16 22:19:06 +0800

    feat: adjust send-email-helper setting.

--- helpers/send-email-helper.js
+++ helpers/send-email-helper.js
@@ -1,9 +1,11 @@
 const nodemailer = require('nodemailer')
 const Mailgen = require('mailgen')
 
+// set Gmail sender info (use gmail's app login info)
 const systemEmail = process.env.EMAIL_ADDRESS
 const emailPassword = process.env.EMAIL_PASSWORD
 
+// set service
 const config = {
   service: 'gmail',
   auth: {
@@ -18,10 +20,11 @@ const sendApplyEmail = (req, result) => {
     theme: 'default',
     product: {
       name: 'OPweb',
-      link: 'https://www.google.com/'
+      link: process.env.PRODUCT_URL
     }
   })
 
+  // set mail content info
   const userName = req.user.name
   const bodyName = result.Category.Users.name ? result.Category.Users.name : 'OP team'
   const bodyItem = result.Category.category
@@ -30,7 +33,7 @@ const sendApplyEmail = (req, result) => {
   const response = {
     body: {
       name: bodyName,
-      intro: '您有來自 OPweb 的申請單',
+      intro: `來自 OPweb: ${userName} 的申請單`,
       table: {
         data: [
           {
@@ -41,30 +44,35 @@ const sendApplyEmail = (req, result) => {
         ]
       },
       action: {
-        instructions: '你可以登入OPweb進行管理',
+        instructions: '你可以登入OPweb進行查看與管理',
         button: {
           color: '#3869D4',
           text: 'Go to OPweb',
-          link: 'https://www.google.com/'
+          link: process.env.PRODUCT_URL // 要記得上線後要改env資料
         }
       },
-      outro: '感謝你的協助處理'
+      outro: '感謝你的協助申請與處理'
     }
   }
 
   const mail = MailGenerator.generate(response)
 
-  const mailTo = result.Category.Users.email ? result.Category.Users.email : 'ray1992.tw@gmail.com'
+  // set message
+  const mailTo = result.Category.Users.email ? result.Category.Users.email : systemEmail
+  const ccTo = req.user.email
   const message = {
     from: systemEmail,
     to: mailTo,
-    subject: '你有一筆OP申請單',
+    subject: 'OPweb申請單',
+    cc: ccTo,
     html: mail
   }
 
   return transporter.sendMail(message)
-    .then(sentMail => console.log(`email sent to ${mailTo}.`))
+    .then(sentMail => console.log(`email sent to manager: ${mailTo} and cc to applicant: ${ccTo}.`))
     .catch(err => Promise.reject(err))
 }
 
-module.exports = { sendApplyEmail }
+module.exports = {
+  sendApplyEmail
+}
